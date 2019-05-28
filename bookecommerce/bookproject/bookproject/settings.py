8537546bd12c68a514da23b909badda73d6edc9a
Satish Pal <34323996+satishpal456@users.noreply.github.com>
2019-05-28 14:59:31 +0530

    Update settings.py

--- bookecommerce/bookproject/bookproject/settings.py
+++ bookecommerce/bookproject/bookproject/settings.py
@@ -167,5 +167,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER='jhavivek260@gmail.com'
-EMAIL_HOST_PASSWORD='Vivekjha@kjit1'
+EMAIL_HOST_USER='add your email'
+EMAIL_HOST_PASSWORD='add your pass'
